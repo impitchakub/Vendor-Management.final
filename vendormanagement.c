60859c17ab67038429f96ec02f6cd99d5f72bd78
impitchakub <68010826@kmitl.ac.th>
2025-09-20 23:54:18 +0700

    feat: add openfile

--- vendormanagement.c
+++ vendormanagement.c
@@ -1,8 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 char SellerName[];
 char ProductType[];
 float Rating[];
 char EvaluationDate[];
+
 int main()
 {
-    FILE*file = fopen("","r")
+        int Rating_table[15][4];
+    {
+
+    }
+
+    {
+        FILE*file=fopen("ระบบจัดการข้อมูลการประเมินผู้ขาย.csv","r");
+        if (file == NULL)
+        {
+            /* code */
+        }   
+    
+        int fclose(file);
+    }
+
 }
