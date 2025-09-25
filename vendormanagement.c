ae7a5f2d1971d95d4b177fc232633233d489b183
impitchakub <68010826@kmitl.ac.th>
2025-09-25 18:51:05 +0700

    fix: Add menu

--- vendormanagement.c
+++ vendormanagement.c
@@ -2,73 +2,117 @@
 #include<stdlib.h>
 #include<string.h>
 
-char SellerName[];
-char ProductType[];
+char SellerName[100];
+char ProductType[100];
 float Rating;
-char EvaluationDate[];
-void menu()
-    {
-    int select;
-    printf("please select the menu");
-    printf("1.Add Seller");
-    printf("2.Search menu");
-    printf("3.Update menu");
-    printf("4.Delete menu");
-    scanf("%d",select);
-    switch (select)
-    {
-        case 1:(Add_menu);
-        break;
-        case 2:(Search_menu);
-        break;
-        case 3:(Update_menu);
-        break;
-        case 4:(Delete_menu);
-        break;
-    }
-    
-    }      
+char EvaluationDate[100];
+
+      
 void Add_menu()
-    {
-        printf("Seller Name");
+    {       
+        {
+            FILE*file=fopen("ระบบจัดการข้อมูลการประเมินผู้ขาย.csv","a");
+            if (file == NULL)
+            {
+                printf("can't open file");   
+            }  
+            fprintf(file,"%s,%s,%f,%s" ,SellerName,ProductType,Rating,EvaluationDate);   
+            fclose(file);
+        }  
+
+        char collect[10];
+        printf("Seller Name : ");
         scanf("%s",SellerName);
-        printf("Product Type");
+        printf("Product Type : ");
         scanf("%s",ProductType);
-        printf("Rating");
-        scanf("%f",Rating);
-        printf("Evaluation Date");
+        printf("Rating : ");
+        scanf("%f",&Rating);
+        printf("Evaluation Date : (DD/MM/YYYY)");
         scanf("%s",EvaluationDate);
+  
+    }
+    /*  printf("save or not (y,n)");
+        scanf("%s",collect);
+        //function save yes or no
+        if (collect== 'y')
+        {
+            Rating_table();
+        }
+        else
+        {
+
+        }
+        int Rating_table[15][4];
+    {
         
     }
-void Search_menu()
-    {
 
+    }
+*/
+/*void Search_menu()
+    {
+        {
+            FILE*file=fopen("ระบบจัดการข้อมูลการประเมินผู้ขาย.csv","r");
+            if (file == NULL)
+            {
+                printf("can't open file");
+            }   
+                fclose(file);
+        }     
     }
 void Update_menu()
     {
-
+        {
+            FILE*file=fopen("ระบบจัดการข้อมูลการประเมินผู้ขาย.csv","r");
+            if (file == NULL)
+            {
+                printf("can't open file");
+            }   
+                fclose(file);
+        }  
     }
 void Delete_menu()
     {
+        {
+            FILE*file=fopen("ระบบจัดการข้อมูลการประเมินผู้ขาย.csv","r");
+            if (file == NULL)
+            {
+                printf("can't open file");
+            }   
+                fclose(file);
+        }  
+    }
 
+    //unit test 2 function
+    //endtoend
+    */
+void menu()
+    {
+
+    int select;
+    printf("please select the menu\n");
+    printf("1.Add Seller\n");
+    printf("2.Search menu\n");
+    printf("3.Update menu\n");
+    printf("4.Delete menu\n");
+    scanf("%d",&select);
+    switch (select)
+    {
+        case 1:Add_menu();
+        break;
+        //case 2:(Search_menu);
+       // break;
+       // case 3:(Update_menu);
+       // break;
+        //case 4:(Delete_menu);
+       // break;
+    }
+    
     }
 int main()
-
 {
-    {
-        FILE*file=fopen("ระบบจัดการข้อมูลการประเมินผู้ขาย.csv","r");
-        if (file == NULL)
-        {
-           printf("can't open file");
-        }   
-         fclose(file);
-    }
-
-        int Rating_table[15][4];
-    {
-
-    }
-
-
+    menu();
+    
 return 0;
+
 }
